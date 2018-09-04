afac56fee41e419e9a0f07e26fa2dac46a56eadf
Stefano Puri <stefano.puri@intecs.it>
2018-09-04 16:35:31 +0200

    updated priority management

--- org.eclipse.capra.generic.priority/src/org/eclipse/capra/generic/priority/DefaultPriorityHandler.java
+++ org.eclipse.capra.generic.priority/src/org/eclipse/capra/generic/priority/DefaultPriorityHandler.java
@@ -15,7 +15,9 @@ import java.util.Collection;
 import org.eclipse.capra.core.handlers.ArtifactHandler;
 import org.eclipse.capra.core.handlers.PriorityHandler;
 import org.eclipse.capra.handler.emf.EMFHandler;
+import org.eclipse.capra.handler.gef.GEFHandler;
 import org.eclipse.capra.handler.papyrus.PapyrusHandler;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gef.EditPart;
 import org.eclipse.papyrus.emf.facet.custom.metamodel.v0_2_0.internal.treeproxy.impl.EObjectTreeElementImpl;
 
@@ -40,13 +42,16 @@ public class DefaultPriorityHandler implements PriorityHandler {
 		// }
 		
 		if (selectedElement instanceof EditPart) {
-				 return handlers.stream().filter(h -> h instanceof EMFHandler).findAny().get();
+				 return handlers.stream().filter(h -> h instanceof GEFHandler).findAny().get();
 				
 		}
 		
 		if (selectedElement instanceof EObjectTreeElementImpl){
 			return handlers.stream().filter(h -> h instanceof PapyrusHandler).findAny().get();
 		}
+		
+		if (selectedElement instanceof EObject)
+			return handlers.stream().filter(h -> h instanceof EMFHandler).findAny().get();
 		
 		return null;
 	}
